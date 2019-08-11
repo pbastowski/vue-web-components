7de592734ecbc7ac8116800612718f8c922fcb01
Paul Bastowski <pbastowski@gmail.com>
2019-08-11 17:01:23 +0200

    initial commit

---
+++ App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1>Vue Web Component</h1>
+        <hr />
+        <form @submit.prevent="addText">
+            <label>
+                Name
+                <input v-model="text" />
+            </label>
+        </form>
+
+        <p v-for="item in texts">
+            {{ item }}
+            <button @click="removeItem(item)">delete</button>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: '',
+                texts: []
+            }
+        },
+
+        created() {
+            this.texts.push('lalala')
+        },
+
+        methods: {
+            addText() {
+                if ('' === this.text.trim()) return
+                this.texts.push(this.text)
+                this.text = ''
+            },
+
+            removeItem(item) {
+                this.texts = this.texts.filter(text => text !== item)
+            }
+        }
+    }
+</script>
